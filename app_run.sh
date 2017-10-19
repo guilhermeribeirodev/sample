b6c21694eedc2df1a25b568a15dedfd94c4dbcf0
guilherme.ribeiro <guilherme.ribeiro@stageintelligence.co.uk>
2017-10-19 12:38:13 +0100

    permissions back

--- app_run.sh
+++ app_run.sh
@@ -4,4 +4,4 @@ echo $APPLICATION_NAME
 echo $LIFECYCLE_EVENT
 
 cd /tmp
-nohup java -jar sample-1.0-jar-with-dependencies.jar &
+nohup java -jar sample-1.0-jar-with-dependencies.jar > /dev/null 2>&1 &
