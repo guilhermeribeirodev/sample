4e3e03f0275db93887e601b61d40eec589f4e5c9
guilherme.ribeiro <guilherme.ribeiro@stageintelligence.co.uk>
2017-10-19 15:12:27 +0100

    fix pom

--- src/main/java/org/test/HelloController.java
+++ src/main/java/org/test/HelloController.java
@@ -1,5 +1,6 @@
 package org.test;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
