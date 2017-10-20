6fc016c65114eba0014c1174207f4ad0208bf25a
guilherme.ribeiro <guilherme.ribeiro@stageintelligence.co.uk>
2017-10-20 11:56:50 +0100

    h3 to h2

--- src/main/java/org/test/HelloController.java
+++ src/main/java/org/test/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "<h3>I'm working with customer :: " + System.getenv("CUSTOMER") + "</h3>";
+        return "<h2>I'm working with customer :: " + System.getenv("CUSTOMER") + "</h2>";
     }
 
 }
